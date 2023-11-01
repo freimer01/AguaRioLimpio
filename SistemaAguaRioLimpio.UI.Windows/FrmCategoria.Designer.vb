3fc18a00d0e3945effd496765fdcbfda67f18f17
freimer01 <131302381+freimer01@users.noreply.github.com>
2023-10-31 22:59:11 -0400

    actualizacion de formulario

--- SistemaAguaRioLimpio.UI.Windows/FrmCategoria.Designer.vb
+++ SistemaAguaRioLimpio.UI.Windows/FrmCategoria.Designer.vb
@@ -1,12 +1,9 @@
-﻿Imports SistemaAguaRioLimpio.BLL
-Imports SistemaAguaRioLimpio.Entities
-
-<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class FrmCategoria
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCategoria
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()>
+    <System.Diagnostics.DebuggerNonUserCode()> _
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -23,127 +20,131 @@ Partial Class FrmCategoria
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()>
+    <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.btnNuevo = New System.Windows.Forms.Button()
-        Me.btnGuardar = New System.Windows.Forms.Button()
-        Me.btnCancelar = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Codigo = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.txtID = New System.Windows.Forms.TextBox()
         Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.bntCancelar = New System.Windows.Forms.Button()
         Me.dgvCategoria = New System.Windows.Forms.DataGridView()
+        Me.ErrorProvider = New System.Windows.Forms.ErrorProvider(Me.components)
         Me.ID = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Nombre = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.ErrorProvider = New System.Windows.Forms.ErrorProvider(Me.components)
         CType(Me.dgvCategoria, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.ErrorProvider, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
+        'Codigo
+        '
+        Me.Codigo.AutoSize = True
+        Me.Codigo.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Codigo.Location = New System.Drawing.Point(11, 47)
+        Me.Codigo.Name = "Codigo"
+        Me.Codigo.Size = New System.Drawing.Size(65, 20)
+        Me.Codigo.TabIndex = 0
+        Me.Codigo.Text = "Codigo"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(11, 86)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(71, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nombre"
+        '
+        'txtID
+        '
+        Me.txtID.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtID.Location = New System.Drawing.Point(100, 44)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(77, 26)
+        Me.txtID.TabIndex = 2
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNombre.Location = New System.Drawing.Point(100, 83)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(215, 26)
+        Me.txtNombre.TabIndex = 3
+        '
         'btnNuevo
         '
         Me.btnNuevo.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnNuevo.Location = New System.Drawing.Point(15, 161)
+        Me.btnNuevo.Location = New System.Drawing.Point(2, 176)
         Me.btnNuevo.Name = "btnNuevo"
-        Me.btnNuevo.Size = New System.Drawing.Size(96, 49)
-        Me.btnNuevo.TabIndex = 0
+        Me.btnNuevo.Size = New System.Drawing.Size(88, 49)
+        Me.btnNuevo.TabIndex = 4
         Me.btnNuevo.Text = "Nuevo"
         Me.btnNuevo.UseVisualStyleBackColor = True
         '
         'btnGuardar
         '
         Me.btnGuardar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnGuardar.Location = New System.Drawing.Point(117, 161)
+        Me.btnGuardar.Location = New System.Drawing.Point(112, 176)
         Me.btnGuardar.Name = "btnGuardar"
-        Me.btnGuardar.Size = New System.Drawing.Size(96, 49)
-        Me.btnGuardar.TabIndex = 1
-        Me.btnGuardar.Text = "Guargar"
+        Me.btnGuardar.Size = New System.Drawing.Size(92, 49)
+        Me.btnGuardar.TabIndex = 5
+        Me.btnGuardar.Text = "Guardar"
         Me.btnGuardar.UseVisualStyleBackColor = True
         '
-        'btnCancelar
+        'bntCancelar
         '
-        Me.btnCancelar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnCancelar.Location = New System.Drawing.Point(219, 161)
-        Me.btnCancelar.Name = "btnCancelar"
-        Me.btnCancelar.Size = New System.Drawing.Size(96, 49)
-        Me.btnCancelar.TabIndex = 2
-        Me.btnCancelar.Text = "Cancelar"
-        Me.btnCancelar.UseVisualStyleBackColor = True
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(16, 38)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(70, 20)
-        Me.Label1.TabIndex = 3
-        Me.Label1.Text = "Codigo "
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(16, 76)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(71, 20)
-        Me.Label2.TabIndex = 4
-        Me.Label2.Text = "Nombre"
-        '
-        'txtID
-        '
-        Me.txtID.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtID.Location = New System.Drawing.Point(92, 35)
-        Me.txtID.Name = "txtID"
-        Me.txtID.Size = New System.Drawing.Size(75, 26)
-        Me.txtID.TabIndex = 5
-        '
-        'txtNombre
-        '
-        Me.txtNombre.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtNombre.Location = New System.Drawing.Point(93, 70)
-        Me.txtNombre.Name = "txtNombre"
-        Me.txtNombre.Size = New System.Drawing.Size(171, 26)
-        Me.txtNombre.TabIndex = 6
+        Me.bntCancelar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bntCancelar.Location = New System.Drawing.Point(223, 176)
+        Me.bntCancelar.Name = "bntCancelar"
+        Me.bntCancelar.Size = New System.Drawing.Size(92, 49)
+        Me.bntCancelar.TabIndex = 6
+        Me.bntCancelar.Text = "Cancelar"
+        Me.bntCancelar.UseVisualStyleBackColor = True
         '
         'dgvCategoria
         '
+        Me.dgvCategoria.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
         Me.dgvCategoria.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.dgvCategoria.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ID, Me.Nombre})
-        Me.dgvCategoria.Location = New System.Drawing.Point(349, 38)
+        Me.dgvCategoria.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter
+        Me.dgvCategoria.Location = New System.Drawing.Point(321, 44)
         Me.dgvCategoria.Name = "dgvCategoria"
-        Me.dgvCategoria.Size = New System.Drawing.Size(316, 172)
+        Me.dgvCategoria.Size = New System.Drawing.Size(329, 181)
         Me.dgvCategoria.TabIndex = 7
+        '
+        'ErrorProvider
+        '
+        Me.ErrorProvider.ContainerControl = Me
         '
         'ID
         '
+        Me.ID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None
         Me.ID.HeaderText = "ID"
         Me.ID.Name = "ID"
+        Me.ID.Width = 50
         '
         'Nombre
         '
         Me.Nombre.HeaderText = "Nombre"
         Me.Nombre.Name = "Nombre"
         '
-        'ErrorProvider
-        '
-        Me.ErrorProvider.ContainerControl = Me
-        '
-        'FrmCategoria
+        'frmCategoria
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(677, 232)
+        Me.ClientSize = New System.Drawing.Size(662, 253)
         Me.Controls.Add(Me.dgvCategoria)
+        Me.Controls.Add(Me.bntCancelar)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.btnNuevo)
         Me.Controls.Add(Me.txtNombre)
         Me.Controls.Add(Me.txtID)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.btnCancelar)
-        Me.Controls.Add(Me.btnGuardar)
-        Me.Controls.Add(Me.btnNuevo)
-        Me.Name = "FrmCategoria"
+        Me.Controls.Add(Me.Codigo)
+        Me.Name = "frmCategoria"
         Me.Text = "Mantenimiento de Categoria"
         CType(Me.dgvCategoria, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.ErrorProvider, System.ComponentModel.ISupportInitialize).EndInit()
@@ -152,70 +153,15 @@ Partial Class FrmCategoria
 
     End Sub
 
-    Friend WithEvents btnNuevo As Button
-    Friend WithEvents btnGuardar As Button
-    Friend WithEvents btnCancelar As Button
-    Friend WithEvents Label1 As Label
+    Friend WithEvents Codigo As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents txtID As TextBox
     Friend WithEvents txtNombre As TextBox
+    Friend WithEvents btnNuevo As Button
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents bntCancelar As Button
     Friend WithEvents dgvCategoria As DataGridView
+    Friend WithEvents ErrorProvider As ErrorProvider
     Friend WithEvents ID As DataGridViewTextBoxColumn
     Friend WithEvents Nombre As DataGridViewTextBoxColumn
-
-    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
-        InicialControles()
-    End Sub
-
-    Private Sub InicialControles()
-        Me.txtID.Text = "0"
-        Me.txtNombre.Clear()
-        Me.dgvCategoria.AutoGenerateColumns = False
-        Me.dgvCategoria.DataSource = CategoriaBLL.GetAll()
-    End Sub
-
-    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
-        '1. Antes de guardar validar los datos'
-        If Not ValidarDatos() Then
-            Exit Sub
-        End If
-        '2. pasar los datos de los controles a un objetos'
-        Dim oCategoria As New CategoriaEntity
-        oCategoria.IdCategoria = Me.txtID.Text
-        oCategoria.Nombre = Me.txtNombre.Text
-        '3. mandamos a guardar el objeto creado a la base de datos'
-        Try
-            CategoriaBLL.Save(oCategoria)
-            MessageBox.Show("Categoria guardada.", "Sistema Agua Rio Limpio", MessageBoxButtons.OK, MessageBoxIcon.Information)
-            InicialControles()
-        Catch ex As Exception
-            MessageBox.Show(ex.Message, "Sistema Agua Rio Limpio", MessageBoxButtons.OK, MessageBoxIcon.Error)
-        End Try
-
-
-    End Sub
-
-    Private Function ValidarDatos() As Boolean
-        Dim resultado As Boolean = True
-
-        'Inicializar los mensajes de las validaciones '
-
-        ErrorProvider.Clear()
-
-        'Verricar que en los campos obligadorios haya datos'
-
-        If String.IsNullOrEmpty(txtNombre.Text) Then
-            ErrorProvider.SetError(txtNombre, "El Nombres es obligatoio")
-            resultado = False
-        End If
-
-        Return resultado
-
-    End Function
-
-    Friend WithEvents ErrorProvider As ErrorProvider
-
-    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
-        Me.Close()
-    End Sub
 End Class
--- SistemaAguaRioLimpio.UI.Windows/FrmCategoria.vb
+++ SistemaAguaRioLimpio.UI.Windows/FrmCategoria.vb
@@ -1,1 +1,56 @@
-﻿
+﻿Imports SistemaAguaRioLimpio.BLL
+Imports SistemaAguaRioLimpio.Entities
+
+Public Class frmCategoria
+    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        InicialControles()
+    End Sub
+
+    Private Sub InicialControles()
+        Me.txtID.Text = "0"
+        Me.txtNombre.Clear()
+        Me.dgvCategoria.AutoGenerateColumns = False
+        Me.dgvCategoria.DataSource = CategoriaBLL.GetAll()
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        '1. Antes de guardar validar los datos'
+        If Not ValidarDatos() Then
+            Exit Sub
+        End If
+        '2. pasar los datos de los controles a un objetos'
+        Dim oCategoria As New CategoriaEntity
+        oCategoria.IdCategoria = Me.txtID.Text
+        oCategoria.Nombre = Me.txtNombre.Text
+        '3. mandamos a guardar el objeto creado a la base de datos'
+        Try
+            CategoriaBLL.Save(oCategoria)
+            MessageBox.Show("Categoria guardada.", "Sistema Agua Rio Limpio", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            InicialControles()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Sistema Agua Rio Limpio", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+    Private Function ValidarDatos() As Boolean
+        Dim resultado As Boolean = True
+
+        'Inicializar los mensajes de las validaciones '
+
+        ErrorProvider.Clear()
+
+        'Verricar que en los campos obligadorios haya datos'
+
+        If String.IsNullOrEmpty(txtNombre.Text) Then
+            ErrorProvider.SetError(txtNombre, "El Nombres es obligatoio")
+            resultado = False
+        End If
+
+        Return resultado
+
+    End Function
+
+    Private Sub bntCancelar_Click(sender As Object, e As EventArgs) Handles bntCancelar.Click
+        Me.Close()
+    End Sub
+End Class
